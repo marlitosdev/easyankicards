387d0daf47f33875160cf0ef1e868a0fbcf64146
Marlon W L Soares <marl.will@hotmail.com>
2026-08-02 20:07:27 -0300

    update 8.21.0
    prompts de correção de cartões foram aprimorados.

--- docs/app.js
+++ docs/app.js
@@ -29,7 +29,7 @@
  *     automática de que todo $("id") existe no index.html.
  */
 
-const VERSAO = "8.20.0";
+const VERSAO = "8.21.0";
 const $ = (id) => document.getElementById(id);
 let ultimoResult = null;
 let previewTimer = null;
